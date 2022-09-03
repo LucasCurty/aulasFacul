e0fd77e2b88b84fcbbfbc77100bc26246cdef549
Lucas Curty <lukas.curty.21@gmail.com>
2022-09-03 16:52:14 -0300

    aulas html css JS

---
+++ JavaScript/modulo2.js
@@ -0,0 +1,12 @@
+// condiçoe
+
+let exe1 = true
+let exe2 = true
+let exe3 = false
+
+
+if( (exe1 && exe2) || exe3){
+  console.log("condiçoes Verdadeira!")
+}else{
+  console.log("condicoes Falsas!")
+}

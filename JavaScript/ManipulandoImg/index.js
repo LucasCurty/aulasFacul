fe231eeb5a415cadcf9b239a6ff66d3352ca9e27
Lucas Curty <lukas.curty.21@gmail.com>
2022-09-23 15:13:10 -0300

    Adicionando js e Python

---
+++ JavaScript/ManipulandoImg/index.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('.img');
+img.src = ('./img.bmp');
+img.width = 500;
+
+
+let pixel = img.getPixel(10,0)
+img.setRed(255)
+img.setBlue(255)
+img.setGreen(255)
---
+++ Python/primeiroCodigo.py
@@ -0,0 +1,17 @@
+print("Eu Amo a Layssa!!!! <3")
+
+# Comentario de uma linha.
+
+""" 
+    Comentario de varias linhas.
+"""
+def mult(numb):
+    global a     # Todas as referências á variável a são para o global
+    a = 2        # variavel de escopo local, sera alterado
+    print(f"Dentro da função, a variável (a) vale: {a}")
+    return a * numb
+
+a = 3 # Variável de escopo global
+b = mult(5)
+print(f"Agora o resultado da multiplicação: {b}")
+print(f"Fora da função, a variável (a) vale: {a}")

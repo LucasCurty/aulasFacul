02aebdf98a3564ee1af40126c455cf98e4d10343
Lucas Curty <74004642+LucasCurty@users.noreply.github.com>
2023-08-19 13:07:59 -0300

    Update tryExcept.py

--- Python/tratando_Erros/tryExcept.py
+++ Python/tratando_Erros/tryExcept.py
@@ -1,9 +1,9 @@
 x = [1,2,3]
 y = 2
 z = 3
-try:
+try: #Tente executar
     print(x+y)
     print(y+z)
     print("Funcionou!")
-except Exception as error:
+except Exception as error: #Se algum erro acima, execute:
     print(f"Foi encontrado o erro {error}")

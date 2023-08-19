405ab0d01d174d4a61aecdd42b48dc39d6cf34ae
Lucas Curty <74004642+LucasCurty@users.noreply.github.com>
2023-08-19 13:06:47 -0300

    Update log.py

--- Python/tratando_Erros/log.py
+++ Python/tratando_Erros/log.py
@@ -1,8 +1,8 @@
 import logging
-#Definindo o formato da mensagem
+#Definindo o formato da mensagem 
 LOG_FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
 
-#configurando o baisco para o arqvuivo log, passando o nome do arquivo, o tipo de prioridade de captura e o formato definido acima
+#Configurando o básico para o arqvuivo log, passando o nome do arquivo [filename], o tipo de prioridade de captura [level] e o formato definido acima [format]
 logging.basicConfig(filename="logs.log", level=logging.DEBUG, format=LOG_FORMAT)
 #criando a capturação dos eventos logs
 log = logging.getLogger()
@@ -13,5 +13,5 @@ log.critical("Erro critico")
 log.error("Holve um erro")
 log.debug("Erro Grave!")
 log.warning("Erro Muito Grave!  ")
-#printando o tipo do level definido no log por padr]ao ele vem 30 = WARNING
+#Imprimindo na tela o tipo do level definido no log, por padrão ele é definido como 30 = WARNING
 print(log.level)
